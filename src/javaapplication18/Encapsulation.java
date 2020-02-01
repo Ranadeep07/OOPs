dab710490d525282f520cf913232155275f41ebb
Ranadeep07 <60144892+Ranadeep07@users.noreply.github.com>
2020-02-01 13:03:11 +0530

    Update Encapsulation.java

--- src/javaapplication18/Encapsulation.java
+++ src/javaapplication18/Encapsulation.java
@@ -1,11 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package javaapplication18;
-
 public class Encapsulation{
      
      public static void main(String[] args) {
